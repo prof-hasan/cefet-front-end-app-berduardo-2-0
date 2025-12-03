6ebd2cce08f893db397a44a848ad509a34118de0
dudubortot <dudu.bortot@gmail.com>
2025-12-02 23:07:13 -0300

    mecanica compra de upgrades

--- scripts.js
+++ scripts.js
@@ -38,41 +38,118 @@ let camposEl = {
     campo7: "imgs/campos/campo7.jpg",
 }
 
-let btnBolaEl = document.querySelector("#bola");
+let bolaEl = document.querySelector("#bola");
 let mostradorEl = document.querySelector("#mostrador");
-let golsPorClique = 50;
-let bolaNivel = 1;
+let golsPorClique = 1;
+let golsPorSegundo = 0;
+let multiplicador = 1;
+
 let totalGols = 0;
 
+let bolaLvl = 1;
 let btnComprarBolaEl = document.querySelector("#comprar-bola");
 let nivelBolaEl = document.querySelector("#nivel-bola");
 let custoBolaEl = document.querySelector("#custo-bola");
 let valorBolaEl = 50;
 
+let chuteiraLvl = 1;
+let chuteiraEl = document.querySelector("#chuteira-img");
 let btnComprarChuteiraEl = document.querySelector("#comprar-chuteira");
-let btnComprarTimeEl = document.querySelector("#comprar-time");
-let btnComprarCampoEl = document.querySelector("#comprar-campo");
+let nivelChuteiraEl = document.querySelector("#nivel-chuteira");
+let custoChuteiraEl = document.querySelector("#custo-chuteira");
+let valorChuteiraEl = 1250;
 
-btnBolaEl.addEventListener("click", () => {
-    totalGols += golsPorClique;
+let timeLvl = 1;
+let timeEl = document.querySelector("#time-img");
+let btnComprarTimeEl = document.querySelector("#comprar-time");
+let nivelTimeEl = document.querySelector("#nivel-time");
+let custoTimeEl = document.querySelector("#custo-time");
+let valorTimeEl = 7500;
+
+
+let btnComprarCampoEl = document.querySelector("#comprar-campo");
+let nivelCampoEl = document.querySelector("#nivel-campo");
+let custoCampoEl = document.querySelector("#custo-campo");
+let valorCampoEl = 23150;
+let campoLvl = 1;
+let campoEl = document.querySelector("#centro");
+
+bolaEl.addEventListener("click", () => {
+    totalGols += golsPorClique * multiplicador;
     mostradorEl.innerHTML = totalGols + " Gols";
 });
 
+setInterval(() => {
+    totalGols += golsPorSegundo;
+    mostradorEl.innerHTML = totalGols + " Gols";
+}, 1000);
+
 btnComprarBolaEl.addEventListener("click", () => {
-    if (totalGols >= valorBolaEl && bolaNivel < 7) {
+    if (totalGols >= valorBolaEl && bolaLvl < 7) {
         totalGols -= valorBolaEl;
-        bolaNivel += 1;
-        if (bolaNivel > 7) {
-            bolaNivel = 7;
+        bolaLvl += 1;
+        if (bolaLvl > 7) {
+            bolaLvl = 7;
         }
 
-        golsPorClique++;
-        nivelBolaEl.innerHTML = "Nível: " + bolaNivel;
-        valorBolaEl *= 3;
+        golsPorClique *= bolaLvl * (bolaLvl -1)
+        nivelBolaEl.innerHTML = "Nível: " + bolaLvl;
+        valorBolaEl = Math.round(valorBolaEl * 4.18);
         custoBolaEl.innerHTML = "Custo: " + (valorBolaEl) + " Gols";
         
+        bolaEl.src = bolasEl["bola" + bolaLvl];
+        mostradorEl.innerHTML = totalGols + " Gols";
+    }
+});
 
-        btnBolaEl.src = bolasEl["bola" + bolaNivel];
+btnComprarChuteiraEl.addEventListener("click", () => {
+    if (totalGols >= valorChuteiraEl && chuteiraLvl < 7) {
+        totalGols -= valorChuteiraEl;
+        chuteiraLvl += 1;
+        if (chuteiraLvl > 7) {
+            chuteiraLvl = 7;
+        }
+        
+        multiplicador += 0.5;
+        nivelChuteiraEl.innerHTML = "Nível: " + chuteiraLvl;
+        valorChuteiraEl = Math.round(valorChuteiraEl * 2.71);
+        custoChuteiraEl.innerHTML = "Custo: " + (valorChuteiraEl) + " Gols";
+        
+        chuteiraEl.src = chuteirasEl["chuteira" + chuteiraLvl];
+        mostradorEl.innerHTML = totalGols + " Gols";
+    }
+});
+
+btnComprarTimeEl.addEventListener("click", () => {
+    if (totalGols >= valorTimeEl && timeLvl < 7) {
+        totalGols -= valorTimeEl;
+        timeLvl += 1;
+        if (timeLvl > 7) {
+            timeLvl = 7;
+        }
+
+        golsPorSegundo += Math.pow(5, timeLvl - 1);
+        nivelTimeEl.innerHTML = "Nível: " + timeLvl;
+        valorTimeEl = Math.round(valorTimeEl * 2.86);
+        custoTimeEl.innerHTML = "Custo: " + (valorTimeEl) + " Gols";
+        timeEl.src = timesEl["time" + timeLvl];
+        mostradorEl.innerHTML = totalGols + " Gols";
+    }
+});
+
+btnComprarCampoEl.addEventListener("click", () => {
+    if (totalGols >= valorCampoEl && campoLvl < 7) {
+        totalGols -= valorCampoEl;
+        campoLvl += 1;
+        if (campoLvl > 7) {
+            campoLvl = 7;
+        }
+        
+        multiplicador += campoLvl * 2;
+        campoEl.style.backgroundImage = `url(${camposEl["campo" + campoLvl]})`;
+        nivelCampoEl.innerHTML = "Nível: " + campoLvl;
+        valorCampoEl = Math.round(valorCampoEl * 3.23);
+        custoCampoEl.innerHTML = "Custo: " + (valorCampoEl) + " Gols";
         mostradorEl.innerHTML = totalGols + " Gols";
     }
 });
